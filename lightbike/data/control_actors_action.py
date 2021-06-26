94704f6c2a3716fd55a0f7a2da8d42ca1e846da0
ethancharles02 <59294780+ethancharles02@users.noreply.github.com>
2021-06-26 09:18:43 -0700

    Added player control code Uncommented the reverse method on point

--- lightbike/data/control_actors_action.py
+++ lightbike/data/control_actors_action.py
@@ -1,9 +1,9 @@
 # get direction from input service, turn player within the cast variable based on the direction
 # restrict direction from moving directly back the way the player is moving (ie. velocity (1, 0) can't suddenly become velocity(-1, 0))
 
-from game import constants
-from game.action import Action
-from game.point import Point
+from data import constants
+from data.action import Action
+from data.point import Point
 
 class ControlActorsAction(Action):
     """A code template for controlling actors. The responsibility of this
@@ -31,10 +31,11 @@ class ControlActorsAction(Action):
             cast (dict): The game actors {key: tag, value: list}.
         """
         direction = self._input_service.get_direction()
-        paddle = cast["paddle"][0] # there's only one in the cast
-        # Checks if the paddle is at either edge of the screen and if the current direction would put it off the screen, if it does, it will set its velocity to 0
-        if (paddle.get_position().get_x() == 1 and direction.get_x() == -1) or (paddle.get_position().get_x() == constants.MAX_X - len(paddle.get_text()) - 1 and direction.get_x() == 1):
-            paddle.set_velocity(Point(0, 0))
-        else:
-            paddle.set_velocity(direction)
-            paddle.set_position(paddle.get_position().add(paddle.get_velocity()))
+        player1 = cast["players"][0]
+
+        player1_velocity = player1.get_velocity()
+        player1_velocity_x = player1_velocity.get_x()
+        player1_velocity_y = player1_velocity.get_y()
+        
+        if not (player1_velocity.reverse().equals(direction) or player1_velocity.equals(direction)):
+            player1.set_velocity(direction)
