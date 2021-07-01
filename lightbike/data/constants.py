9adebe5217987cf0d77a164d60765a0482e362f9
Fenrir468 <crowson.avery@gmail.com>
2021-07-01 13:53:50 -0600

    small txt fixes

--- lightbike/data/constants.py
+++ lightbike/data/constants.py
@@ -1,4 +1,5 @@
-# Week 2: background color: Black arcade.color.BLACK
+
+from arcade import color
 
 SCREEN_TITLE = "Lightbike"
 SCREEN_WIDTH = 800
@@ -7,3 +8,4 @@ SPRITE_SCALING = 1
 MOVEMENT_SPEED = 100
 AI_RANGE = 5
 AI_BLINDSPOT_RANGE = 3 
+BACKGROUND_COLOR = color.BLACK
