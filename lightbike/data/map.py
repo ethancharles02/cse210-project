3760d674c7f5700e62f6f0abae350eab5203de02
ethancharles02 <59294780+ethancharles02@users.noreply.github.com>
2021-07-05 08:02:01 -0700

    Updated Plan

--- lightbike/data/map.py
+++ lightbike/data/map.py
@@ -1,3 +1,4 @@
 """
 The map class will be used to hold map designs for the game
 """
+# Add a function that takes grid coordinates to add walls to the map, we may need to specify the size of the grid as well (ie. 20 x 15)
