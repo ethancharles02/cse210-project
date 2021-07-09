bad810bd64ff231bc8c15c6c3796c563468661b4
Fenrir468 <crowson.avery@gmail.com>
2021-07-08 21:24:15 -0600

    started test player

---
+++ lightbike/test_player.py
@@ -0,0 +1,17 @@
+import pytest
+import arcade
+from data.lightbike import Lightbike
+from data import constants
+
+def test_set_movement_speed():
+       
+
+def test_get_keys():
+        
+
+def test_set_keys():
+        
+
+def test_update_keys():
+
+pytest.main(["-v", "--tb=no", "test_player.py"])
