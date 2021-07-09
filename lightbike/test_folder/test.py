844010035f9a857b9c958d8f90ec1bb62291197c
richabbott3 <rich.abbott3@gmail.com>
2021-07-09 15:15:31 -0600

    only test

--- lightbike/test_folder/test.py
+++ lightbike/test_folder/test.py
@@ -1,6 +1,11 @@
-from arcade import Sound
-SOUND_BACKGROUND = Sound("test.mp3")
+# from arcade import Sound
+# SOUND_BACKGROUND = Sound("test.mp3")
 
-SOUND_BACKGROUND.play(0.2, loop=True)
+# SOUND_BACKGROUND.play(0.2, loop=True)
 
-cancel_input = input()
+
+import pyglet
+source = pyglet.media.load('test.mp3', streaming=False)
+source.play()
+pyglet.app.run()
+# cancel_input = input()
