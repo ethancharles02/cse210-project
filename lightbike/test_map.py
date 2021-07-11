0ad21f489109fbe0ffc37a33b87b4faf45aa1f44
richabbott3 <rich.abbott3@gmail.com>
2021-07-10 18:29:39 -0600

    created test_map.py

---
+++ lightbike/test_map.py
@@ -0,0 +1,30 @@
+import pytest
+# import arcade
+from data.player import Player
+from arcade import key, Sprite
+from data import constants
+
+def test_set_map():
+    pass
+
+def test_get_map():
+    pass
+
+def test_set_mapx():
+    pass
+
+def test_get_mapx():
+    pass
+
+def test_setmapy():
+    pass
+
+def test_get_mapy():
+    pass
+
+def test_return_sprite_map():
+    pass
+
+
+
+pytest.main(["-v", "--tb=no", "test_map.py"])
