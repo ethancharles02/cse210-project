6d2091f93f7975f46e160ce08d2f1b612e9c631a
ethancharles02 <59294780+ethancharles02@users.noreply.github.com>
2021-07-13 16:01:23 -0700

    Code cleanup

--- lightbike/data/particles.py
+++ lightbike/data/particles.py
@@ -5,6 +5,7 @@ import math
 import arcade
 from data import constants
 
+# Pulled from https://arcade.academy/examples/sprite_explosion_particles.html#sprite-explosion-particles
 
 class Particle(arcade.SpriteCircle):
     """
@@ -12,52 +13,40 @@ class Particle(arcade.SpriteCircle):
     def __init__(self, my_list):
         """
         """
-        # Choose a random color
         color = random.choice(constants.PARTICLE_COLORS)
 
-        # Make the particle
         super().__init__(constants.PARTICLE_RADIUS, color)
 
-        # Track normal particle texture, so we can 'flip' when we sparkle.
         self.normal_texture = self.texture
 
-        # Keep track of the list we are in, so we can add a smoke trail
         self.my_list = my_list
 
-        # Set direction/speed
         speed = random.random() * constants.PARTICLE_SPEED_RANGE + constants.PARTICLE_MIN_SPEED
         direction = random.randrange(360)
         self.change_x = math.sin(math.radians(direction)) * speed
         self.change_y = math.cos(math.radians(direction)) * speed
 
-        # Track original alpha. Used as part of 'sparkle' where we temp set the
-        # alpha back to 255
         self.my_alpha = 255
 
-        # What list do we add smoke particles to?
         self.my_list = my_list
 
     def update(self):
         """ Update the particle """
         if self.my_alpha <= constants.PARTICLE_FADE_RATE:
-            # Faded out, remove
             self.remove_from_sprite_lists()
         else:
-            # Update
             self.my_alpha -= constants.PARTICLE_FADE_RATE
             self.alpha = self.my_alpha
             self.center_x += self.change_x
             self.center_y += self.change_y
             self.change_y -= constants.PARTICLE_GRAVITY
 
-            # Should we sparkle this?
             if random.random() <= constants.PARTICLE_SPARKLE_CHANCE:
                 self.alpha = 255
                 self.texture = arcade.make_circle_texture(self.width, arcade.color.WHITE)
             else:
                 self.texture = self.normal_texture
 
-            # Leave a smoke particle?
             if random.random() <= constants.SMOKE_CHANCE:
                 smoke = Smoke(5)
                 smoke.position = self.position
@@ -74,10 +63,8 @@ class Smoke(arcade.SpriteCircle):
     def update(self):
         """ Update this particle """
         if self.alpha <= constants.PARTICLE_FADE_RATE:
-            # Remove faded out particles
             self.remove_from_sprite_lists()
         else:
-            # Update values
             if self.alpha - constants.SMOKE_FADE_RATE < 0:
                 self.alpha = 0
             else:
